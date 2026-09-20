36b795f24f9c90820624f76e14419011c17954fd
fabien-gigante <fabien.gigante@free.fr>
2026-09-20 08:59:17 +0200

    Rename mod

--- src/client/java/com/fabien_gigante/VisibleGenomeClient.java
+++ src/client/java/com/fabien_gigante/VisibleGenomeClient.java
@@ -2,7 +2,7 @@ package com.fabien_gigante;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class VisiblePandaGenesClient implements ClientModInitializer {
+public class VisibleGenomeClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/com/fabien_gigante/VisibleGenomeMod.java
+++ src/main/java/com/fabien_gigante/VisibleGenomeMod.java
@@ -5,8 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class VisiblePandaGenesMod implements ModInitializer {
-	public static final String MOD_ID = "visible-panda-genes";
+public class VisibleGenomeMod implements ModInitializer {
+	public static final String MOD_ID = "visible-genome";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
